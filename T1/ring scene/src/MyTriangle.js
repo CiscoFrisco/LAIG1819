2c746de57b04eda25fb6ce91a8746e1391134b86
Unknown <pedromiguel.fernandes17@gmail.com>
2018-10-09 14:24:11 +0100

    started MyTriangle

--- T1/ring scene/src/MyTriangle.js
+++ T1/ring scene/src/MyTriangle.js
@@ -14,13 +14,12 @@ class MyTriangle extends CGFobject {
 	 * @param {Number} minT minimum t texture coordinate
 	 * @param {Number} maxT maximum t texture coordinate
 	 */
-	constructor(scene, minS = 0, maxS = 1, minT = 0, maxT = 1) {
+	constructor(scene, x1, y1, z1, x2, y2, z2, x3, y3, z3) {
 		super(scene);
 
-		this.minS = minS;
-		this.maxS = maxS;
-		this.minT = minT;
-		this.maxT = maxT;
+		this.v1 = vec3.fromValues(x1, y1, z1);
+		this.v2 = vec3.fromValues(x2, y2, z2);
+		this.v3 = vec3.fromValues(x3, y3, z3);
 
 		this.initBuffers();
 	};
@@ -29,28 +28,46 @@ class MyTriangle extends CGFobject {
 	 * Initializes vertices, indices, normals and texture coordinates
 	 */
 	initBuffers() {
-		this.vertices = [-0.5, -0.5, 0,
-			0.5, -0.5, 0, -0.5, 0.5, 0,
+		this.vertices = [
+			this.v1[0], this.v1[1], this.v1[2],
+			this.v2[0], this.v2[1], this.v2[2],
+			this.v3[0], this.v3[1], this.v3[2],
 		];
 
 		this.indices = [
-            0, 1, 2,
-            0, 2, 1,
+			0, 1, 2,
 		];
 
-		this.texCoords = [
-			this.minS, this.maxT,
-			this.maxS, this.maxT,
-			this.minS, this.minT,
+		var v21 = vec3.fromValues(this.v2[0] - this.v1[0], this.v2[1] - this.v1[1], this.v2[2] - this.v1[2]);
+		var v32 = vec3.fromValues(this.v3[0] - this.v2[0], this.v3[1] - this.v2[1], this.v3[2] - this.v3[2]); //vetor do ponto 2 ao ponto 3
+		var n = vec3.create()
+		vec3.cross(n, v21, v32);
+		vec3.normalize(n, n);
+
+		this.normals = [
+			n[0], n[1], n[2],
+			n[0], n[1], n[2],
+			n[0], n[1], n[2],
 		];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
-
-		this.normals = [0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-		];
-
 		this.initGLBuffers();
 	};
+
+	updateTexCoords(length_s, length_t) {
+		var a = Math.sqrt(Math.pow(this.v1[0] - this.v3[0], 2) + Math.pow(this.v1[1] - this.v3[1], 2) + Math.pow(this.v1[2] - this.v3[2], 2));
+		var b = Math.sqrt(Math.pow(this.v2[0] - this.v1[0], 2) + Math.pow(this.v2[1] - this.v1[1], 2) + Math.pow(this.v2[2] - this.v1[2], 2));
+		var c = Math.sqrt(Math.pow(this.v3[0] - this.v2[0], 2) + Math.pow(this.v3[1] - this.v2[1], 2) + Math.pow(this.v3[2] - this.v2[2], 2));
+
+		var beta = Math.acos((Math.pow(a, 2) - Math.pow(b, 2) + Math.pow(c, 2)) / (2 * a * c));
+
+		this.texCoords = [
+			0, v/length_t,
+			c/length_s, v/length_t,
+			(c - a * Math.cos(beta))/length_s, (v - a * Math.sin(beta))/length_t,
+		];
+
+		this.updateTexCoordsGLBuffers();
+
+	}
 };
