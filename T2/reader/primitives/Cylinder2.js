10c769f16869531904f68999627133e7242f8e21
Francisco Filipe <francilipe.7@gmail.com>
2018-11-07 22:38:56 +0000

    trying to fix cylinder2

--- T2/reader/primitives/Cylinder2.js
+++ T2/reader/primitives/Cylinder2.js
@@ -14,7 +14,7 @@ class Cylinder2 extends CGFobject {
 		this.initControlPoints();
 		console.log(this.nPointsV);
 		console.log(this.controlPoints);
-		var nurbsSurface = new CGFnurbsSurface(this.nPointsU - 1, this.nPointsV - 1, this.controlPoints);
+		var nurbsSurface = new CGFnurbsSurface(this.nPointsU - 1, this.nPointsV, this.controlPoints);
 		this.obj = new CGFnurbsObject(this.scene, this.stacks, this.slices, nurbsSurface);
 	   };
 	   
@@ -26,26 +26,31 @@ class Cylinder2 extends CGFobject {
 	   initControlPoints(){
 
 		var allControlPoints = [];
-		var inc = (this.base - this.top)/(this.nPointsU - 1);
-		var angInc = 2*Math.PI/(this.slices - 1);
+		var radius = this.base;
+		var final_radius = this.top;
 		var ang = 2*Math.PI;
-		var radius = this.top;
+		var angInc = 2*Math.PI/(this.slices);
+		console.log(angInc*180/Math.PI);
 		var z = 0;
-		var zInc = this.height/(this.nPointsU - 1);
+		var final_z = this.height;
 
 		for(let u = 0; u < this.nPointsU; u++){
 			var controlPointsU = [];
-			for(let j = 0; j < this.nPointsV; j++){
+			for(let j = 0; j <= this.nPointsV; j++){
 				controlPointsU.push([Math.cos(ang)*radius, Math.sin(ang)*radius, z, 1]);
 				ang -= angInc;
 			}
-		  allControlPoints.push(controlPointsU);
-		  ang = 2*Math.PI;
-		  radius += inc;
-		  z+=zInc;
+		  
+			allControlPoints.push(controlPointsU);
+		  	ang = 2*Math.PI;
+		  	radius = final_radius;
+		  	z = final_z;
 		}
 
 		this.controlPoints = allControlPoints;
+		console.log(this.controlPoints);
 	   }
+
+	  
 
 }
